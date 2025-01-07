db04ced2118eb373365772fcd3610bf86dcbf608
aaron-sandoval <32021231+aaron-sandoval@users.noreply.github.com>
2025-01-06 18:59:44 -0700

    wip Phragmen

--- generative_social_choice/slates/voting_algorithms.py
+++ generative_social_choice/slates/voting_algorithms.py
@@ -12,15 +12,35 @@ from generative_social_choice.utils.helper_functions import (
 from generative_social_choice.queries.query_interface import Agent, Generator
 
 
-def seq_phragmen_maximin(
+def phragmen_update_assignments(
+    assignments: pd.DataFrame,
+    rated_votes: pd.DataFrame,
+    candidate: int,
+) -> pd.DataFrame:
+    """
+    Update the assignments of the voters to the candidates.
+    """
+    new_assignments = assignments.copy()
+    reassigned = assignments["candidate_id"] > new_assignments["utility"] # It matters if this is > or >= for int-valued utilities
+    old_assignments = new_assignments.loc[reassigned].copy()
+    new_assignments.loc[reassigned, "candidate_id"] = candidate
+    new_assignments.loc[reassigned, "utility"] = rated_votes[candidate]
+
+
+    new_assignments.loc[reassigned, "load"] += 1
+    
+    return new_assignments
+
+
+def seq_phragmen_minimax_rated(
     rated_votes: pd.DataFrame,
     slate_size: int,
     egalitarian_utilitarian: float = 1.0,
 ) -> tuple[list[int], pd.Series]:
     """
-    Sequential Phragmen Maximin Voting Algorithm
+    Sequential Phragmen Maximin Algorithm for rated voting.
 
-    Adaptation of the Sequential Phragmen Maximin Voting Algorithm to rated voting.
+    Adaptation of the Sequential Phragmen Minimax Voting Algorithm to rated voting.
 
     # Arguments
     - `rated_votes: pd.DataFrame`: Utility of each voter (rows) for each candidate (columns)
@@ -34,15 +54,19 @@ def seq_phragmen_maximin(
     # TODO: Figure out egalitarian_utilitarian
 
     # Initialize the slate and assignments
-    slate = []
-    assignments = pd.DataFrame(
+    slate: list[int] = []
+    assignments: pd.DataFrame = pd.DataFrame(
         index=rated_votes.index,
-        columns=["candidate_id", "load"],
-        dtype={"candidate_id": int, "load": float}
+        columns=["candidate_id", "load", "utility"],
+        dtype={"candidate_id": int, "load": float, "utility": float}
     )
 
     for i in range(slate_size):
         min_load = float("inf")
         min_load_candidate_id: int = -1
         for candidate in rated_votes.columns:
-            candidate_load = 
+            aissignments_with_candidate = phragmen_update_assignments(
+                assignments,
+                rated_votes,
+                candidate,
+            )
