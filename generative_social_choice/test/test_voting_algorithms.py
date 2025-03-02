58110963d0a0d6310e527a1ee4397a7edf80968d
Aaron <aaron.sandoval10@gmail.com>
2025-03-02 19:54:14 +0000

    Added `rated_vote_case.augmented_cases` to `TestVotingAlgorithmAgainstAxioms`. Must pass all augmented cases to pass test. No change in logging

--- generative_social_choice/test/test_voting_algorithms.py
+++ generative_social_choice/test/test_voting_algorithms.py
@@ -151,8 +151,12 @@ class TestVotingAlgorithmAgainstAxioms(unittest.TestCase):
 
         for axiom in axioms_to_evaluate:
             with self.subTest(msg=_NAME_DELIMITER.join([voting_algorithm.name, rated_vote_case.name, axiom.name])):
-                assert axiom.evaluate_assignment(rated_votes=rated_vote_case.rated_votes, slate_size=rated_vote_case.slate_size, assignments=assignments), \
-                    f"Slate {slate} does not satisfy {axiom.name}"
+                for rated_votes in rated_vote_case.augmented_cases():
+                    assert axiom.evaluate_assignment(
+                        rated_votes=rated_votes, 
+                        slate_size=rated_vote_case.slate_size, 
+                        assignments=assignments
+                    ), f"Slate {slate} does not satisfy {axiom.name}"
 
 
 class TestVotingAlgorithmAssignments(unittest.TestCase):
