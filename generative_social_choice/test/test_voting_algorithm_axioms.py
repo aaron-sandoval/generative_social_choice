11245842a3bc1ce7c14381b6bd65857a51891f7a
aaron-sandoval <32021231+aaron-sandoval@users.noreply.github.com>
2025-04-18 10:54:46 -0600

    Added `TestNonRadicalMinUtilityAxiom`. It's passing

--- generative_social_choice/test/test_voting_algorithm_axioms.py
+++ generative_social_choice/test/test_voting_algorithm_axioms.py
@@ -23,6 +23,7 @@ from generative_social_choice.slates.voting_algorithm_axioms import (
     MinimumAndTotalUtilityParetoAxiom,
     NonRadicalTotalUtilityAxiom,
     VotingAlgorithmAxiom,
+    NonRadicalMinUtilityAxiom,
 )
 from generative_social_choice.test.utilities_for_testing import rated_vote_cases
 
@@ -51,6 +52,42 @@ class TestNonRadicalTotalUtilityAxiom(unittest.TestCase):
     ])
     def test_non_radical_total_utility_axiom(self, rated_vote_case: RatedVoteCase, expected_slates: list[list[str]], max_tradeoff: Optional[float] = None):
         axiom = NonRadicalTotalUtilityAxiom(max_tradeoff=max_tradeoff) if max_tradeoff is not None else NonRadicalTotalUtilityAxiom()
+        self.assertEqual(axiom.satisfactory_slates(rated_vote_case.rated_votes, rated_vote_case.slate_size), {frozenset(slate) for slate in expected_slates})
+
+        for slate in expected_slates:
+            self.assertTrue(axiom.evaluate_assignment(rated_vote_case.rated_votes, rated_vote_case.slate_size, voter_max_utilities_from_slate(rated_vote_case.rated_votes, slate)))
+
+        # Check that all other slates are invalid
+        invalid_slates = set(frozenset(slate) for slate in itertools.combinations(rated_vote_case.rated_votes.columns, r=rated_vote_case.slate_size))
+        invalid_slates.difference_update({frozenset(slate) for slate in expected_slates})
+        for slate in invalid_slates:
+            self.assertFalse(axiom.evaluate_assignment(rated_vote_case.rated_votes, rated_vote_case.slate_size, voter_max_utilities_from_slate(rated_vote_case.rated_votes, slate)))
+
+class TestNonRadicalMinUtilityAxiom(unittest.TestCase):
+    @parameterized.expand([
+        (
+            rated_vote_cases["Ex 4.2"],
+            [["s1", "s2"]], 
+            None
+        ),
+        (
+            rated_vote_cases["Ex Alg1.4"],
+            [["s2"]], 
+            None
+        ),
+        (
+            rated_vote_cases["Ex 2.1"],
+            [["s1", "s2"]],
+            None
+        ),
+        (
+            rated_vote_cases["Ex 2.2"],
+            [["s1", "s2"], ["s2", "s4"]],
+            None
+        ),            
+    ])
+    def test_non_radical_min_utility_axiom(self, rated_vote_case: RatedVoteCase, expected_slates: list[list[str]], max_tradeoff: Optional[float] = None):
+        axiom = NonRadicalMinUtilityAxiom(max_tradeoff=max_tradeoff) if max_tradeoff is not None else NonRadicalMinUtilityAxiom()
         self.assertEqual(axiom.satisfactory_slates(rated_vote_case.rated_votes, rated_vote_case.slate_size), {frozenset(slate) for slate in expected_slates})
 
         for slate in expected_slates:
